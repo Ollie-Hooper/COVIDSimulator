6cdfff5ab1752dbf3c633730ea06f2f2414e940f
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2021-03-22 15:32:55 +0000

    Updated from meeting today

---
+++ covid_sim/city.py
@@ -0,0 +1,12 @@
+class City:
+
+    def __init__(self):
+        self.population
+        self.susceptible
+        self.infected
+        self.recovered
+        self.dead
+
+    def timestep(self):
+        # Oscar's code
+        pass

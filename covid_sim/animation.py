2f384db971317cf41964b41271deae90144dcde6
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2021-03-25 20:07:08 +0000

    Rearranged files

---
+++ covid_sim/animation.py
@@ -0,0 +1,165 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+class Animation:
+    """Create an animation of the epidemic.
+
+    This creates a plot figure with two subplots:
+
+        A colormap showing the status of the people.
+        A line plot showing how many people are in each status.
+
+    The resulting animation can either be shown on screen (with show()) or can
+    be saved to a file (with save()).
+
+    Example
+    =======
+
+    Create a simulation and animate it for 100 days showing the animation on
+    screen:
+
+    >>> sim = Simulation(10, 10, recovery=0.1, infection=0.2, death=0.05)
+    >>> sim.infect_randomly(3)  # infect three people (chosen randomly)
+    >>> anim = Animation(simulation, 100)
+    >>> animshow()
+
+    """
+
+    def __init__(self, simulation, duration):
+        self.simulation = simulation
+        self.duration = duration
+
+        self.figure = plt.figure(figsize=(8, 4))
+        self.axes_grid = self.figure.add_subplot(1, 2, 1)
+        self.axes_line = self.figure.add_subplot(1, 2, 2)
+
+        self.gridanimation = GridAnimation(self.axes_grid, self.simulation)
+        self.lineanimation = LineAnimation(self.axes_line, self.simulation, duration)
+
+    def show(self):
+        """Run the animation on screen"""
+        animation = FuncAnimation(self.figure, self.update, frames=range(100),
+                                  init_func=self.init, blit=True, interval=200)
+        plt.show()
+
+    def save(self, filename):
+        """Run the animation and save to a video"""
+
+        # NOTE: needs ffmpeg installed and on PATH
+        animation = FuncAnimation(self.figure, self.update, frames=range(100),
+                                  init_func=self.init, blit=True, interval=300)
+        animation.save(filename, fps=30, extra_args=['-vcodec', 'libx264'])
+
+    def init(self):
+        """Initialise the animation (called by FuncAnimation)"""
+        # We could generalise this to a loop and then it would work for any
+        # numer of *animation objects.
+        actors = []
+        actors += self.gridanimation.init()
+        actors += self.lineanimation.init()
+        return actors
+
+    def update(self, framenumber):
+        """Update the animation (called by FuncAnimation)"""
+        self.simulation.update()
+        actors = []
+        actors += self.gridanimation.update(framenumber)
+        actors += self.lineanimation.update(framenumber)
+        return actors
+
+
+class GridAnimation:
+    """Animate a grid showing status of people at each position"""
+
+    def __init__(self, axes, simulation):
+        self.axes = axes
+        self.simulation = simulation
+        rgb_matrix = self.simulation.get_rgb_matrix()
+        self.image = self.axes.imshow(rgb_matrix)
+        self.axes.set_xticks([])
+        self.axes.set_yticks([])
+
+    def init(self):
+        return self.update(0)
+
+    def update(self, framenum):
+        day = framenum
+        rgb_matrix = self.simulation.get_rgb_matrix()
+        self.image.set_array(rgb_matrix)
+        return [self.image]
+
+
+class LineAnimation:
+    """Animate a line series showing numbers of people in each status"""
+
+    def __init__(self, axes, simulation, duration):
+        self.axes = axes
+        self.simulation = simulation
+        self.duration = duration
+        self.xdata = []
+        self.ydata = {status: [] for status in simulation.STATUSES}
+        self.line_mpl = {}
+        for status, colour in simulation.COLOURMAP.items():
+            [line] = self.axes.plot([], [], color=colour, label=status, linewidth=2)
+            self.line_mpl[status] = line
+        self.axes.legend(prop={'size': 'x-small'}, loc='center right')
+        self.axes.set_xlabel('days')
+        self.axes.set_ylabel('%', rotation=0)
+
+    def init(self):
+        self.axes.set_xlim([0, self.duration])
+        self.axes.set_ylim([0, 100])
+        return []
+
+    def update(self, framenum):
+        percents = self.simulation.get_percentage_status()
+        self.xdata.append(len(self.xdata))
+        for status, percent in percents.items():
+            self.ydata[status].append(percent)
+            self.line_mpl[status].set_data(self.xdata, self.ydata[status])
+        return list(self.line_mpl.values())
+
+
+def plot_simulation(simulation, duration):
+    """Produce a plot showing grid status at different points in time.
+
+    Creates a 5x3 grid of subplots showing the status at 15 different days
+    throughout the simulation.
+
+    Example
+    =======
+
+    >>> sim = Simulation(10, 10, recovery=0.1, infection=0.2, death=0.05)
+    >>> sim.infect_randomly(3)  # infect three people (chosen randomly)
+    >>> fig = plot_simulation(simulation, 100)
+    >>> plt.show()
+
+    """
+    # NOTE: Maybe this should be configurable e.g. could be W and H or (W, H)
+    # arguments to plot_simulation.
+    W, H = 5, 3
+    N = W * H
+
+    fig = plt.figure()
+    axes = fig.subplots(nrows=H, ncols=W).flat
+
+    # Fix the spacing between subplots:
+    fig.subplots_adjust(wspace=0.1, hspace=0.4)
+
+    # Try to find days that are approximately equally spaced although N might
+    # not divide duration exactly.
+    days = [(duration * i) // (N - 1) for i in range(N)]
+
+    for ax, day in zip(axes, days):
+        while simulation.day < day:
+            simulation.update()
+        rgb_matrix = simulation.get_rgb_matrix()
+        ax.imshow(rgb_matrix)
+        ax.set_title('Day ' + str(day))
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    # Return the figure. The caller of this function can decide whether to use
+    # show (screen) or savefig (file).
+    return fig
--- covid_sim/simulator.py
+++ covid_sim/simulator.py
@@ -32,77 +32,13 @@ NOTE: You need to install ffmpeg for the above to work. The ffmpeg program
 must also be on PATH.
 """
 
-
-import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 from numpy.random import random, randint
 
 
-def main(*args):
-    """Command line entry point.
-
-    $ python simulator.py                        # show animation on screen
-    $ python simulator.py --file=video.mp4       # save animation to video
-    $ python simulator.py --plot                 # show plot on screen
-    $ python simulator.py --plot --file=plot.pdf # save plot to pdf
-
-    """
-    #
-    # Use argparse to handle parsing the command line arguments.
-    #   https://docs.python.org/3/library/argparse.html
-    #
-    parser = argparse.ArgumentParser(description='Animate an epidemic')
-    parser.add_argument('--size', metavar='N', type=int, default=50,
-                        help='Use a N x N simulation grid')
-    parser.add_argument('--duration', metavar='T', type=int, default=100,
-                        help='Simulate for T days')
-    parser.add_argument('--recovery', metavar='P', type=float, default=0.1,
-                        help='Probability of recovery (per day)')
-    parser.add_argument('--infection', metavar='P', type=float, default=0.1,
-                        help='Probability of infecting a neighbour (per day)')
-    parser.add_argument('--death', metavar='P', type=float, default=0.005,
-                        help='Probability of dying when infected (per day)')
-    parser.add_argument('--cases', metavar='N', type=int, default=2,
-                        help='Number of initial infected people')
-    parser.add_argument('--plot', action='store_true',
-                        help='Generate plots instead of an animation')
-    parser.add_argument('--file', metavar='N', type=str, default=None,
-                        help='Filename to save to instead of showing on screen')
-    args = parser.parse_args(args)
-
-    # Set up the simulation
-    simulation = Simulation(args.size, args.size,
-                            args.recovery, args.infection, args.death)
-    simulation.infect_randomly(args.cases)
-
-    # Plot or animation?
-    if args.plot:
-        fig = plot_simulation(simulation, args.duration)
-
-        if args.file is None:
-            #  python simulator.py --plot
-            plt.show()
-        else:
-            #  python simulator.py --plot --file=plot.pdf
-            fig.savefig(args.file)
-    else:
-        animation = Animation(simulation, args.duration)
-
-        if args.file is None:
-            #  python simulator.py
-            animation.show()
-        else:
-            #  python simulator.py --file=animation.mp4
-            #
-            # NOTE: this needs ffmpeg to be installed.
-            animation.save(args.file)
-
-
-#----------------------------------------------------------------------------#
-#                  Class design                                              #
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+#                   Class design                                              #
+# ----------------------------------------------------------------------------#
 
 # There are a number of classes here which could be combined in different
 # ways.  The idea is that the same classes could be reused for other things.
@@ -134,9 +70,9 @@ def main(*args):
 # and init() methods.
 
 
-#----------------------------------------------------------------------------#
-#                  Simulation class                                          #
-#----------------------------------------------------------------------------#
+# ----------------------------------------------------------------------------#
+#                   Simulation class                                          #
+# ----------------------------------------------------------------------------#
 
 
 class Simulation:
@@ -283,8 +219,8 @@ class Simulation:
 
         # Need to be careful about people at the edge of the grid.
         # ivals and jvals are the coordinates of neighbours around i, j
-        ivals = range(max(i-1, 0), min(i+2, self.width))
-        jvals = range(max(j-1, 0), min(j+2, self.height))
+        ivals = range(max(i - 1, 0), min(i + 2, self.width))
+        jvals = range(max(j - 1, 0), min(j + 2, self.height))
         number = 0
         for ip in ivals:
             for jp in jvals:
@@ -323,178 +259,11 @@ class Simulation:
         return rgb_matrix
 
 
-#----------------------------------------------------------------------------#
-#                  Animation classes                                         #
-#----------------------------------------------------------------------------#
-
-class Animation:
-    """Create an animation of the epidemic.
-
-    This creates a plot figure with two subplots:
-
-        A colormap showing the status of the people.
-        A line plot showing how many people are in each status.
-
-    The resulting animation can either be shown on screen (with show()) or can
-    be saved to a file (with save()).
-
-    Example
-    =======
-
-    Create a simulation and animate it for 100 days showing the animation on
-    screen:
-
-    >>> sim = Simulation(10, 10, recovery=0.1, infection=0.2, death=0.05)
-    >>> sim.infect_randomly(3)  # infect three people (chosen randomly)
-    >>> anim = Animation(simulation, 100)
-    >>> animshow()
-
-    """
-
-    def __init__(self, simulation, duration):
-        self.simulation = simulation
-        self.duration = duration
-
-        self.figure = plt.figure(figsize=(8, 4))
-        self.axes_grid = self.figure.add_subplot(1, 2, 1)
-        self.axes_line = self.figure.add_subplot(1, 2, 2)
-
-        self.gridanimation = GridAnimation(self.axes_grid, self.simulation)
-        self.lineanimation = LineAnimation(self.axes_line, self.simulation, duration)
-
-    def show(self):
-        """Run the animation on screen"""
-        animation = FuncAnimation(self.figure, self.update, frames=range(100),
-                init_func = self.init, blit=True, interval=200)
-        plt.show()
-
-    def save(self, filename):
-        """Run the animation and save to a video"""
-
-        # NOTE: needs ffmpeg installed and on PATH
-        animation = FuncAnimation(self.figure, self.update, frames=range(100),
-                init_func = self.init, blit=True, interval=300)
-        animation.save(filename, fps=30, extra_args=['-vcodec', 'libx264'])
-
-
-    def init(self):
-        """Initialise the animation (called by FuncAnimation)"""
-        # We could generalise this to a loop and then it would work for any
-        # numer of *animation objects.
-        actors = []
-        actors += self.gridanimation.init()
-        actors += self.lineanimation.init()
-        return actors
-
-    def update(self, framenumber):
-        """Update the animation (called by FuncAnimation)"""
-        self.simulation.update()
-        actors = []
-        actors += self.gridanimation.update(framenumber)
-        actors += self.lineanimation.update(framenumber)
-        return actors
-
-
-class GridAnimation:
-    """Animate a grid showing status of people at each position"""
-
-    def __init__(self, axes, simulation):
-        self.axes = axes
-        self.simulation = simulation
-        rgb_matrix = self.simulation.get_rgb_matrix()
-        self.image = self.axes.imshow(rgb_matrix)
-        self.axes.set_xticks([])
-        self.axes.set_yticks([])
-
-    def init(self):
-        return self.update(0)
-
-    def update(self, framenum):
-        day = framenum
-        rgb_matrix = self.simulation.get_rgb_matrix()
-        self.image.set_array(rgb_matrix)
-        return [self.image]
-
-
-class LineAnimation:
-    """Animate a line series showing numbers of people in each status"""
-
-    def __init__(self, axes, simulation, duration):
-        self.axes = axes
-        self.simulation = simulation
-        self.duration = duration
-        self.xdata = []
-        self.ydata = {status: [] for status in simulation.STATUSES}
-        self.line_mpl = {}
-        for status, colour in simulation.COLOURMAP.items():
-            [line] = self.axes.plot([], [], color=colour, label=status, linewidth=2)
-            self.line_mpl[status] = line
-        self.axes.legend(prop={'size':'x-small'}, loc='center right')
-        self.axes.set_xlabel('days')
-        self.axes.set_ylabel('%', rotation=0)
-
-    def init(self):
-        self.axes.set_xlim([0, self.duration])
-        self.axes.set_ylim([0, 100])
-        return []
-
-    def update(self, framenum):
-        percents = self.simulation.get_percentage_status()
-        self.xdata.append(len(self.xdata))
-        for status, percent in percents.items():
-            self.ydata[status].append(percent)
-            self.line_mpl[status].set_data(self.xdata, self.ydata[status])
-        return list(self.line_mpl.values())
-
-
-def plot_simulation(simulation, duration):
-    """Produce a plot showing grid status at different points in time.
-
-    Creates a 5x3 grid of subplots showing the status at 15 different days
-    throughout the simulation.
-
-    Example
-    =======
-
-    >>> sim = Simulation(10, 10, recovery=0.1, infection=0.2, death=0.05)
-    >>> sim.infect_randomly(3)  # infect three people (chosen randomly)
-    >>> fig = plot_simulation(simulation, 100)
-    >>> plt.show()
-
-    """
-    # NOTE: Maybe this should be configurable e.g. could be W and H or (W, H)
-    # arguments to plot_simulation.
-    W, H = 5, 3
-    N = W*H
-
-    fig = plt.figure()
-    axes = fig.subplots(nrows=H, ncols=W).flat
-
-    # Fix the spacing between subplots:
-    fig.subplots_adjust(wspace=0.1, hspace=0.4)
-
-    # Try to find days that are approximately equally spaced although N might
-    # not divide duration exactly.
-    days = [(duration * i) // (N - 1) for i in range(N)]
-
-    for ax, day in zip(axes, days):
-        while simulation.day < day:
-            simulation.update()
-        rgb_matrix = simulation.get_rgb_matrix()
-        ax.imshow(rgb_matrix)
-        ax.set_title('Day ' + str(day))
-        ax.set_xticks([])
-        ax.set_yticks([])
-
-    # Return the figure. The caller of this function can decide whether to use
-    # show (screen) or savefig (file).
-    return fig
-
-
 if __name__ == "__main__":
     #
     # CLI entry point. The main() function can also be imported and called
     # with string arguments.
     #
     import sys
+
     main(*sys.argv[1:])

b2b3024d824dd024ee991bc2f54d8670b9d8788a
Ollie Hooper <35010178+Ollie-Hooper@users.noreply.github.com>
2021-05-13 12:33:53 +0100

    Changed layout to one column

--- web_app/layout.py
+++ web_app/layout.py
@@ -5,103 +5,95 @@ import dash_bootstrap_components as dbc
 
 def get_layout(defaults):
     return dbc.Container([
-        dbc.Row([
-            dbc.Col([
-                dbc.Container([
-                    dbc.InputGroup([
-                        dbc.InputGroupAddon("Size", addon_type="prepend"),
-                        dbc.Input(value=defaults["size"], type="number", min=0, step=1, id="num-size"),
-                    ]),
-                    dbc.InputGroup([
-                        dbc.InputGroupAddon("Duration", addon_type="prepend"),
-                        dbc.Input(value=defaults["duration"], type="number", min=0, step=1, id="num-duration"),
-                    ]),
-                    dbc.InputGroup([
-                        dbc.InputGroupAddon("Initial cases", addon_type="prepend"),
-                        dbc.Input(value=defaults["cases"], type="number", min=0, step=1, id="num-cases"),
-                    ]),
-                    dbc.Button(
-                        "Probabilities",
-                        id="btn-probabilities",
-                        color="secondary",
-                    ),
-                    dbc.Collapse([
-                        dbc.Card([
-                            dbc.CardBody([
+        dbc.Container([
+            dbc.InputGroup([
+                dbc.InputGroupAddon("Size", addon_type="prepend"),
+                dbc.Input(value=defaults["size"], type="number", min=0, step=1, id="num-size"),
+            ]),
+            dbc.InputGroup([
+                dbc.InputGroupAddon("Duration", addon_type="prepend"),
+                dbc.Input(value=defaults["duration"], type="number", min=0, step=1, id="num-duration"),
+            ]),
+            dbc.InputGroup([
+                dbc.InputGroupAddon("Initial cases", addon_type="prepend"),
+                dbc.Input(value=defaults["cases"], type="number", min=0, step=1, id="num-cases"),
+            ]),
+            dbc.Button(
+                "Probabilities",
+                id="btn-probabilities",
+                color="secondary",
+            ),
+            dbc.Collapse([
+                dbc.Card([
+                    dbc.CardBody([
+                        dbc.Row([
+                            dbc.Col([
+                                dbc.Label(prob),
                                 dbc.Row([
                                     dbc.Col([
-                                        dbc.Label(prob),
-                                        dbc.Row([
-                                            dbc.Col([
-                                                dbc.InputGroup([
-                                                    dbc.InputGroupAddon(f"< {age}", addon_type="prepend"),
-                                                    dbc.Input(value=p, type="number", min=0, step=0.01,
-                                                              id=f"num-{prob}-{age}"),
-                                                ])
-                                            ]) for age, p in probs.items()
+                                        dbc.InputGroup([
+                                            dbc.InputGroupAddon(f"< {age}", addon_type="prepend"),
+                                            dbc.Input(value=p, type="number", min=0, step=0.01,
+                                                      id=f"num-{prob}-{age}"),
                                         ])
-                                    ]) for prob, probs in defaults["probabilities"].items()
+                                    ]) for age, p in probs.items()
                                 ])
-                            ])
-                        ])
-                    ], id="clp-probabilities")
-                ])
-            ]),
-            dbc.Col([
-                dbc.Container([
-                    dbc.Button(
-                        "Vaccinator",
-                        id="btn-vaccinator",
-                        color="secondary",
-                    ),
-                    dbc.Collapse([
-                        dbc.Card([
-                            dbc.CardBody([
-                                dbc.InputGroup([
-                                    dbc.InputGroupAddon("Start day", addon_type="prepend"),
-                                    dbc.Input(value=defaults["vaccinator"]["start"], type="number", min=0, step=1,
-                                              id="num-vaccinator-start"),
-                                ]),
-                                dbc.InputGroup([
-                                    dbc.InputGroupAddon("Vaccination capacity increase rate (per day)",
-                                                        addon_type="prepend"),
-                                    dbc.Input(value=defaults["vaccinator"]["rate"], type="number", min=0, step=0.01,
-                                              id="num-vaccinator-rate"),
-                                ]),
-                                dbc.InputGroup([
-                                    dbc.InputGroupAddon("Max vaccination capacity (per day)", addon_type="prepend"),
-                                    dbc.Input(value=defaults["vaccinator"]["max"], type="number", min=0, step=1,
-                                              id="num-vaccinator-max"),
-                                ]),
-                            ])
-                        ])
-                    ], id="clp-vaccinator")
-                ])
-            ]),
-            dbc.Col([
-                dbc.Container([
-                    dbc.Row([
-                        dbc.Col([
-                            dbc.Button("Run/Save animation", color="primary", id="btn-anim")
-                        ]),
-                        dbc.Col([
-                            dbc.Input(placeholder="(Optional) File name to save animation", id="txt-anim-fname")
-                        ])
-                    ]),
-                    dbc.Row([
-                        dbc.Col([
-                            dbc.Button("Plot", color="secondary", id="btn-plot")
-                        ]),
-                        dbc.Col([
-                            dbc.Input(placeholder="(Optional) File name to save plot", id="txt-plot-fname")
-                        ])
-                    ]),
-                    dbc.Row([
-                        dbc.Col([
-                            dbc.Label(id='lbl-status')
+                            ]) for prob, probs in defaults["probabilities"].items()
                         ])
                     ])
-                ], fluid=True)
+                ])
+            ], is_open=True, id="clp-probabilities")
+        ]),
+        dbc.Container([
+            dbc.Button(
+                "Vaccinator",
+                id="btn-vaccinator",
+                color="secondary",
+            ),
+            dbc.Collapse([
+                dbc.Card([
+                    dbc.CardBody([
+                        dbc.InputGroup([
+                            dbc.InputGroupAddon("Start day", addon_type="prepend"),
+                            dbc.Input(value=defaults["vaccinator"]["start"], type="number", min=0, step=1,
+                                      id="num-vaccinator-start"),
+                        ]),
+                        dbc.InputGroup([
+                            dbc.InputGroupAddon("Vaccination capacity increase rate (per day)",
+                                                addon_type="prepend"),
+                            dbc.Input(value=defaults["vaccinator"]["rate"], type="number", min=0, step=0.01,
+                                      id="num-vaccinator-rate"),
+                        ]),
+                        dbc.InputGroup([
+                            dbc.InputGroupAddon("Max vaccination capacity (per day)", addon_type="prepend"),
+                            dbc.Input(value=defaults["vaccinator"]["max"], type="number", min=0, step=1,
+                                      id="num-vaccinator-max"),
+                        ]),
+                    ])
+                ])
+            ], is_open=True, id="clp-vaccinator")
+        ]),
+        dbc.Container([
+            dbc.Row([
+                dbc.Col([
+                    dbc.Button("Run/Save animation", color="primary", id="btn-anim")
+                ]),
+                dbc.Col([
+                    dbc.Input(placeholder="(Optional) File name to save animation", id="txt-anim-fname")
+                ])
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Button("Plot", color="secondary", id="btn-plot")
+                ]),
+                dbc.Col([
+                    dbc.Input(placeholder="(Optional) File name to save plot", id="txt-plot-fname")
+                ])
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.Label(id='lbl-status')
+                ])
             ])
         ])
-    ], fluid=True)
+    ])
